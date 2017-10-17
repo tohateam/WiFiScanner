aa94d34b480825f43dff0edb3adfc5753f0e2e9b
tohateam <tohaguk@outlook.com>
2017-10-17 15:55:41 +0300

    Добавьте файлы проекта.

---
+++ WifiScanner/Main.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace WifiScanner
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveLogToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.listViewAccessPoints = new System.Windows.Forms.ListView();
+            this.SSID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.MAC = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SignalQuality = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.dbm = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Channel = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Encryption = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Authentication = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(750, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveLogToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveLogToolStripMenuItem
+            // 
+            this.saveLogToolStripMenuItem.Name = "saveLogToolStripMenuItem";
+            this.saveLogToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.saveLogToolStripMenuItem.Text = "Save Log";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(118, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 431);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(750, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 24);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listViewAccessPoints);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(750, 407);
+            this.splitContainer1.SplitterDistance = 152;
+            this.splitContainer1.TabIndex = 7;
+            // 
+            // listViewAccessPoints
+            // 
+            this.listViewAccessPoints.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.SSID,
+            this.MAC,
+            this.SignalQuality,
+            this.dbm,
+            this.Channel,
+            this.Encryption,
+            this.Authentication});
+            this.listViewAccessPoints.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listViewAccessPoints.Location = new System.Drawing.Point(0, 0);
+            this.listViewAccessPoints.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.listViewAccessPoints.Name = "listViewAccessPoints";
+            this.listViewAccessPoints.Size = new System.Drawing.Size(750, 152);
+            this.listViewAccessPoints.TabIndex = 4;
+            this.listViewAccessPoints.UseCompatibleStateImageBehavior = false;
+            this.listViewAccessPoints.View = System.Windows.Forms.View.Details;
+            // 
+            // SSID
+            // 
+            this.SSID.Text = "SSID";
+            this.SSID.Width = 124;
+            // 
+            // MAC
+            // 
+            this.MAC.Text = "MAC";
+            this.MAC.Width = 124;
+            // 
+            // SignalQuality
+            // 
+            this.SignalQuality.Text = "SignalQuality";
+            this.SignalQuality.Width = 96;
+            // 
+            // dbm
+            // 
+            this.dbm.Text = "dbm";
+            this.dbm.Width = 80;
+            // 
+            // Channel
+            // 
+            this.Channel.Text = "Channel";
+            this.Channel.Width = 78;
+            // 
+            // Encryption
+            // 
+            this.Encryption.Text = "Encryption";
+            this.Encryption.Width = 96;
+            // 
+            // Authentication
+            // 
+            this.Authentication.Text = "Authentication";
+            this.Authentication.Width = 124;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(750, 251);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox1_Paint);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 453);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(766, 600);
+            this.Name = "Main";
+            this.Text = "Wi-Fi Scanner";
+            this.Load += new System.EventHandler(this.FormScanner_Load);
+            this.Shown += new System.EventHandler(this.FormScanner_Shown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveLogToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListView listViewAccessPoints;
+        private System.Windows.Forms.ColumnHeader SSID;
+        private System.Windows.Forms.ColumnHeader MAC;
+        private System.Windows.Forms.ColumnHeader SignalQuality;
+        private System.Windows.Forms.ColumnHeader dbm;
+        private System.Windows.Forms.ColumnHeader Channel;
+        private System.Windows.Forms.ColumnHeader Encryption;
+        private System.Windows.Forms.ColumnHeader Authentication;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WifiScanner/Main.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WiFiScanner;
+
+namespace WifiScanner
+{
+    public partial class Main : Form
+    {
+        private WlanClient wlanClient = null;
+
+        #region Старт программы *******************************************************************
+
+        public Main()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+            this.listViewAccessPoints.Columns[2].TextAlign = HorizontalAlignment.Center;
+            this.listViewAccessPoints.Columns[3].TextAlign = HorizontalAlignment.Center;
+            this.listViewAccessPoints.Columns[4].TextAlign = HorizontalAlignment.Center;
+            this.listViewAccessPoints.Columns[5].TextAlign = HorizontalAlignment.Center;
+            this.listViewAccessPoints.Columns[6].TextAlign = HorizontalAlignment.Center;
+        }
+
+        private void FormScanner_Load(object sender, EventArgs e)
+        {
+            wlanClient = new WlanClient();
+
+            Taskbar.SetState(this.Handle, Taskbar.TaskbarStates.Indeterminate);
+
+            this.listViewAccessPoints.ListViewItemSorter = new ListViewColumnSorter();
+            this.listViewAccessPoints.ColumnClick += new ColumnClickEventHandler(ListViewHelper.ListView_ColumnClick);
+        }
+
+        private void FormScanner_Shown(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.WaitCursor;
+
+            try {
+                this.ScanRegister();
+            } catch (Exception ex) {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            this.Cursor = Cursors.Default;
+        }
+
+        #endregion
+
+        #region Сканирование сетей ****************************************************************
+
+        private void ScanRegister()
+        {
+            listViewAccessPoints.Items.Clear();
+
+            foreach (WlanClient.WlanInterface wlanInterface in wlanClient.Interfaces) {
+                Wlan.WlanAvailableNetwork[] networks =
+                    wlanInterface.GetAvailableNetworkList(Wlan.WlanGetAvailableNetworkFlags.IncludeAllAdhocProfiles
+                    | Wlan.WlanGetAvailableNetworkFlags.IncludeAllManualHiddenProfiles);
+                Wlan.WlanBssEntry[] wlanBssEntries = wlanInterface.GetNetworkBssList();
+
+                wlanInterface.WlanNotification +=
+                    new WlanClient.WlanInterface.WlanNotificationEventHandler(Wlan_Notification);
+
+                this.NetworkList(networks, wlanBssEntries);
+            }
+        }
+
+        private void Scan()
+        {
+            listViewAccessPoints.Items.Clear();
+
+            foreach (WlanClient.WlanInterface wlanInterface in wlanClient.Interfaces) {
+                Wlan.WlanAvailableNetwork[] networks = wlanInterface.GetAvailableNetworkList(Wlan.WlanGetAvailableNetworkFlags.IncludeAllAdhocProfiles |
+                                                                                              Wlan.WlanGetAvailableNetworkFlags.IncludeAllManualHiddenProfiles);
+                Wlan.WlanBssEntry[] wlanBssEntries = wlanInterface.GetNetworkBssList();
+
+                this.NetworkList(networks, wlanBssEntries);
+            }
+        }
+
+        private void NetworkList(Wlan.WlanAvailableNetwork[] networks, Wlan.WlanBssEntry[] wlanBssEntries)
+        {
+            int count = 0;
+
+            foreach (Wlan.WlanAvailableNetwork network in networks) {
+                Application.DoEvents();
+
+                Wlan.WlanBssEntry entry = (from bs in wlanBssEntries
+                                           where GetProfileName(bs.dot11Ssid).Trim() == GetProfileName(network.dot11Ssid).Trim()
+                                           select bs).FirstOrDefault<Wlan.WlanBssEntry>();
+
+                ShowProgress((count += 1), networks.Length, Taskbar.TaskbarStates.Normal);
+                this.AddToList(network, entry);
+
+            }
+
+            ShowProgress(0, 0, Taskbar.TaskbarStates.NoProgress);
+            pictureBox1.Invalidate();
+        }
+
+        private void AddToList(Wlan.WlanAvailableNetwork network, Wlan.WlanBssEntry entry)
+        {
+            foreach (ListViewItem lvi in this.listViewAccessPoints.Items) {
+                if (lvi.SubItems[1].Text == this.GetMacAddress(entry.dot11Bssid)) {
+                    return;
+                }
+            }
+
+            ListViewItem wifiItem = new ListViewItem(this.GetProfileName(network.dot11Ssid));
+
+            // MAC Address
+            wifiItem.SubItems.Add(this.GetMacAddress(entry.dot11Bssid));
+
+            // Signal Quality
+            wifiItem.SubItems.Add(string.Format("{0}%", network.wlanSignalQuality.ToString()));
+            //ChartValuePercent = (int)network.wlanSignalQuality;
+
+            // dBm Value
+            wifiItem.SubItems.Add(string.Format("{0}dBm", entry.rssi.ToString()));
+            //ChartValueDbm = (int)entry.rssi * -1;
+
+            // Channel No
+            wifiItem.SubItems.Add(this.GetChannel(entry).ToString());
+            //ChartChannelX = GetChannel(entry);
+
+            // Encryption
+            wifiItem.SubItems.Add(network.dot11DefaultCipherAlgorithm.ToString());
+            // Authentication
+            wifiItem.SubItems.Add(network.dot11DefaultAuthAlgorithm.ToString());
+
+            int range = ((int)network.wlanSignalQuality - 1) / 25;
+            wifiItem.ImageIndex = range;
+
+            if (network.dot11DefaultCipherAlgorithm.ToString().Equals("None"))
+                wifiItem.BackColor = Color.LimeGreen;
+
+            listViewAccessPoints.Items.Add(wifiItem);
+        }
+
+        private void ResetList(bool reset)
+        {
+            if (reset) listViewAccessPoints.Items.Clear();
+            listViewAccessPoints.Refresh();
+        }
+
+        #endregion
+
+        #region Вспомогательные методы ************************************************************
+
+        private string GetProfileName(Wlan.Dot11Ssid value)
+        {
+            return Encoding.UTF8.GetString(value.SSID, 0, (int)value.SSIDLength);
+        }
+
+        private string GetMacAddress(Byte[] value)
+        {
+            var macAddrLen = (uint)value.Length;
+            var str = new string[(int)macAddrLen];
+
+            for (int i = 0; i < macAddrLen; i++)
+                str[i] += (value[i].ToString("x2").PadLeft(2, '0').ToUpper() + ":").Trim();
+
+            str[str.Length - 1] = str[str.Length - 1].Remove(2, 1);
+
+            return string.Join("", str);
+        }
+
+        private int GetChannel(Wlan.WlanBssEntry value)
+        {
+            int freq = (int)(value.chCenterFrequency / 1000);
+
+            if (freq >= 2412 && freq <= 2484)
+                return (freq - 2412) / 5 + 1;
+            else if (freq >= 5170 && freq <= 5825)
+                return (freq - 5170) / 5 + 34;
+            else
+                return -1;
+        }
+
+        private void ShowProgress(int value, int max, Taskbar.TaskbarStates status)
+        {
+            if (status == Taskbar.TaskbarStates.NoProgress) {
+                Taskbar.SetState(this.Handle, Taskbar.TaskbarStates.NoProgress);
+                return;
+            }
+
+            Taskbar.SetValue(this.Handle, value, max);
+            Taskbar.SetState(this.Handle, status);
+        }
+
+        private void Wlan_Notification(Wlan.WlanNotificationData notifyData)
+        {
+            switch (notifyData.notificationSource) {
+                case Wlan.WlanNotificationSource.ACM:
+                    switch ((Wlan.WlanNotificationCodeAcm)notifyData.NotificationCode) {
+                        case Wlan.WlanNotificationCodeAcm.ScanComplete:
+
+                            this.Cursor = Cursors.WaitCursor;
+
+                            try {
+                                this.Scan();
+                            } catch (Exception ex) {
+                                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            }
+
+                            this.Cursor = Cursors.Default;
+
+                            break;
+                    }
+                    break;
+            }
+        }
+
+        #endregion
+
+        #region Рисование графика каналов *********************************************************
+
+        #endregion
+
+        private void PictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            #region Инициализация графика *********************************************************
+            // Включаем антиалиасис
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            // Шрифт меток осей
+            var drawFont = new Font("Arial", 6);
+            // Шрифт имени Wi-Fi точки
+            var drawFontSsid = new Font("Arial", 8);
+            // Цвет текста
+            var drawBrush = new SolidBrush(Color.White);
+            // Формат текста
+            var drawFormat = new StringFormat {
+                FormatFlags = StringFormatFlags.DisplayFormatControl
+            };
+
+            // Цвет координатных линий
+            var axisXYPen = new Pen(Color.Red, 2);
+            // цвет дополнительных линий
+            var axisMinorPen = new Pen(ColorTranslator.FromHtml("#FF909090"), 1) {
+                DashStyle = DashStyle.DashDotDot
+            };
+
+            // Смещение осей графика относительно размеров pictureBox1
+            const float axisShift = 20;
+            // Размер области рисования - (Ширина/Высота - смещение)
+            float graphicsWidth = (pictureBox1.Width - axisShift);
+            float graphicsHeight = (pictureBox1.Height - axisShift);
+            // Шаг дополнительных линий X-16 каналов, Y-10 линий
+            float axisMinorXstep = graphicsWidth / 16;
+            float axisMinorYstep = graphicsHeight / 10;
+            // Размер шага для дуги (1 деление сетки)
+            float arcYstep = graphicsHeight / 100;
+            // Ширина дуги = 4-м делениям оси Х
+            float widthArc = axisMinorXstep * 4;
+
+            #endregion
+
+            #region Рисием сетку графика **********************************************************
+
+            // Рисуем горизонтальные линии (уровень сигнала)
+            float y = graphicsHeight;
+            for (int i = 0; i < 10; i++) {
+                if (i == 0) {
+                    // Абцисса X, начинаем от фактического 0
+                    e.Graphics.DrawLine(axisXYPen, 0, y, graphicsWidth + axisShift, y);
+                } else {
+                    // Доюолнительные линиии
+                    e.Graphics.DrawLine(axisMinorPen, axisShift, y, graphicsWidth + axisShift, y);
+                    // Метки линий (10%, 20% ...)
+                    e.Graphics.DrawString((i * 10).ToString() + "%", drawFont, drawBrush, 0, y - 6, drawFormat);
+                }
+                y -= axisMinorYstep;
+            }
+
+            // Рисуем горизонтальные линии (номер канала)
+            float x = axisShift;
+            for (int i = 0; i < 16; i++) {
+                if (i == 0) {
+                    // Абцисса Y, начинаем от фактического 0
+                    e.Graphics.DrawLine(axisXYPen, x, 0, x, graphicsHeight + axisShift);
+                } else {
+                    e.Graphics.DrawLine(axisMinorPen, x, graphicsHeight, x, 0);
+                    e.Graphics.DrawString(i.ToString(), drawFont, drawBrush, x - 3, graphicsHeight + 5, drawFormat);
+                }
+                x += axisMinorXstep;
+            }
+
+            #endregion
+
+
+        }
+    }
+}
